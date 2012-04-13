c2bd9710d94594f4ea6f6bd9b193fde1c2c33c2b
Scott Fenwick <Armflailer@gmail.com>
2012-04-13 11:36:06 -0400

    Client

---
+++ Client/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "ClientSocket.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    int port = 35533;
+
+	if(argc == 2){
+		port = atoi(argv[1]);
+	}
+
+    bool done = false;
+    string ipaddress;
+
+    cout << "Server IP Address: ";
+    getline(cin,ipaddress);
+
+    //cin.ignore(1000,'\n');
+
+    ClientSocket sockClient;
+
+    if(sockClient.ConnectToServer(ipaddress.c_str(),port)){
+        if(sockClient.Login()){
+            while(!done){
+                done = sockClient.Command();
+            }
+        }
+
+    }
+
+
+
+    system("Pause");
+    return 0;
+}
